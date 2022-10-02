82d5332d29954d8c459c1e4ea9138e873ce0f018
azaibMeer <azaibmir1292@gmail.com>
2022-10-02 12:36:44 +0500

    work in contact us

--- news/app/Http/Controllers/ContactUsController.php
+++ news/app/Http/Controllers/ContactUsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Contact;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
 
 class ContactUsController extends Controller
 {
@@ -14,7 +15,8 @@ class ContactUsController extends Controller
      */
     public function index()
     {
-       return view("layouts.contactUs");
+       $data['categories'] = Category::get();
+       return view("layouts.contactUs",$data);
     }
 
     /**
--- news/routes/web.php
+++ news/routes/web.php
@@ -5,24 +5,14 @@ use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('layouts.welcome');
-// });
+
+
+
+
 Route::get('/', [NewsController::class,'index']);
 Route::get('/contactUs', [ContactUsController::class,'index']);
 
 
- Route::get('/detail', function () {
-     return view('layouts.detail');
- });
+ 
+

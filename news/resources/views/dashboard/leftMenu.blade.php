72dcb3af9418af70f6cd358afd82071fc30f8d60
azaibMeer <azaibmir1292@gmail.com>
2022-11-02 23:49:33 +0500

    today changes

--- news/resources/views/dashboard/leftMenu.blade.php
+++ news/resources/views/dashboard/leftMenu.blade.php
@@ -3,27 +3,24 @@
                 <ul class="nav metismenu" id="side-menu">
                     <li class="nav-header">
                         <div class="dropdown profile-element">
-                            <img alt="image" class="rounded-circle" src="img/profile_small.jpg"/>
+                            <img alt="image" class="rounded-circle" src="{{Auth::User()->image}}" style="width:55px"/>
                             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                                <span class="block m-t-xs font-bold">David Williams</span>
-                                <span class="text-muted text-xs block">Art Director <b class="caret"></b></span>
+                                <span class="block m-t-xs font-bold" style="font-size: 20px;">{{Auth::User()->name}}</span>
+                                
                             </a>
-                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                <li><a class="dropdown-item" href="profile.html">Profile</a></li>
-                                <li><a class="dropdown-item" href="contacts.html">Contacts</a></li>
-                                <li><a class="dropdown-item" href="mailbox.html">Mailbox</a></li>
-                                <li class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="login.html">Logout</a></li>
-                            </ul>
+                            
 
                         </div>
                         <div class="logo-element">
                             IN+
                         </div>
                     </li>
-                    
+                    <li>
+                        <a href="{{url('/dashboard')}}"><i class="fa fa-dashboard"></i>Dashboard</span></a>
+                       
+                    </li>
                      <li>
-                        <a href="#"><i class="fa fa-table"></i> <span class="nav-label">News</span><span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-newspaper-o"></i> <span class="nav-label">News</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
                             <li><a href="{{url('/news/create')}}">Add</a></li>
                             <li><a href="{{url('/news/list')}}">List</a></li>
@@ -39,7 +36,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-list-alt"></i> <span class="nav-label">Cities</span><span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-building"></i> <span class="nav-label">Cities</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
                             <li><a href="{{url('/cities/create')}}">Add</a></li>
                             <li><a href="{{url('/cities/list')}}">List</a></li>
@@ -48,7 +45,7 @@
                     </li>
                     
                     <li>
-                        <a href="#"><i class="fa fa-table"></i> <span class="nav-label">Verses</span><span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-quote-left"></i> <span class="nav-label">Verses</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
                             
                             <li><a href="{{url('/verses/edit')}}">update</a></li>
@@ -62,7 +59,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-table"></i> <span class="nav-label">Reporter</span><span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-user-circle"></i> <span class="nav-label">Reporter</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
                             <li><a href="{{url('/reporter/create')}}">Add</a></li>
                             <li><a href="{{url('/reporter/list')}}">List</a></li>
@@ -70,7 +67,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-table"></i> <span class="nav-label">Contact Us</span><span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-phone-square"></i> <span class="nav-label">Contact Us</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
                             
                             <li><a href="{{url('/contact/list')}}">List</a></li>
@@ -78,7 +75,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-table"></i> <span class="nav-label">Ads</span><span class="fa arrow"></span></a>
+                        <a href="#"><i class="fas fa-ad"></i> <span class="nav-label">Ads</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
                             
                             <li><a href="{{url('/ads/create')}}">Add</a></li>

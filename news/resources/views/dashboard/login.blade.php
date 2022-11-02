ced659fa13639966bafdc8f12a43d6e6d6ee746a
azaibMeer <azaibmir1292@gmail.com>
2022-11-03 00:17:14 +0500

    create acount remove

--- news/resources/views/dashboard/login.blade.php
+++ news/resources/views/dashboard/login.blade.php
@@ -43,9 +43,8 @@
                 </div>
                 <button type="submit" class="btn btn-primary block full-width m-b" value="login">Login</button>
 
-                <a href="#"><small>Forgot password?</small></a>
-                <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+                
+                
             </form>
            
         </div>
